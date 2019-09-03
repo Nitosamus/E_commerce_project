ef9572ab7e6fd88256b0a74ace15722ddaeb648c
Tonie <toniemontfort@gmail.com>
2019-09-03 06:51:40 +0300

    deuxieme commit sur heroku autre model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
 resources :items, only: [:show, :index]
-=======
 
   resources :items
-
->>>>>>> a2aa0cf038701500ad6ba5a4a4b1e5d354b95e02
 end
