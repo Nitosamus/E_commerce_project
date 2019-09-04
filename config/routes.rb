73a0285d7f3b080fd06ac2c52b7ecac3c953eed6
Tonie <toniemontfort@gmail.com>
2019-09-04 11:12:56 +0300

    partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
 resources :items, only: [:show, :index]
-  resources :charges
+resources :charges
+resources :users
 end
