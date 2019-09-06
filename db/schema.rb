942486ccf908fd8eacb2cc003139d00b3cde88c1
Tonie <toniemontfort@gmail.com>
2019-09-06 06:44:24 +0300

    mmmmm

--- db/schema.rb
+++ db/schema.rb
@@ -72,10 +72,6 @@ ActiveRecord::Schema.define(version: 2019_09_05_112629) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "last_name"
-    t.string "first_name"
-    t.integer "age"
-    t.string "city"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
