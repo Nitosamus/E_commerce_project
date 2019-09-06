5e29f52b9d00209ab0393e440a596ae2dd75a72d
Tonie <toniemontfort@gmail.com>
2019-09-06 09:05:21 +0300

    edit user

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "items#index"
 root to: "items#index"
 	resources :items, only: [:show, :index] do
 		resources :carts, only: [:create, :show, :update] do
 				resources :charges, only: [:new, :create, :index]
 		end
 	end
+	resources :users do
+		resources :images
+	end
 resources :orders, only: [:create, :show, :index]
-resources :users
 end
